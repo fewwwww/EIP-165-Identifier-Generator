9cbf2cf88c21551b091e4a8d03b05e496d473000
Suning Yao <sy2940@nyu.edu>
2023-05-10 14:13:19 -0400

    Fix multiple line function parsing

--- src/utils/parseInterfaceABI.js
+++ src/utils/parseInterfaceABI.js
@@ -1,11 +1,29 @@
 export const parseInterfaceABI = (interfaceCode) => {
-  let interfaceCodeLines = interfaceCode.split(String.fromCharCode(10));
+  let interfaceCodeLines = interfaceCode.split('\n');
+
+  // The Output
+  let interfaceFunctionLines = [];
+
   for (let i = 0; i < interfaceCodeLines.length; i++) {
     interfaceCodeLines[i] = interfaceCodeLines[i].trim();
+
+    // If it is a multiple line function, combine them into one line.
+    if (interfaceCodeLines[i].startsWith('function') && !interfaceCodeLines[i].endsWith(';')) {
+      for (let j = i + 1; j < interfaceCodeLines.length; j++) {
+        // Need extra trim because lines after index i are not trimmed yet.
+        interfaceCodeLines[i] += interfaceCodeLines[j].trim();
+        if (interfaceCodeLines[j].endsWith(';')) {
+          break;
+        }
+      }
+    }
+
+    // If it is a function line (optionally after combination of multiple line),
+    // push it to the output.
+    if (interfaceCodeLines[i].startsWith('function') && interfaceCodeLines[i].endsWith(';')) {
+      interfaceFunctionLines.push(interfaceCodeLines[i].replace(';', ''));
+    }
   }
-  interfaceCodeLines = interfaceCodeLines.filter((line) =>
-    line.startsWith('function'),
-  );
-  interfaceCodeLines = interfaceCodeLines.map((line) => line.replace(';', ''));
-  return interfaceCodeLines;
+
+  return interfaceFunctionLines;
 };
